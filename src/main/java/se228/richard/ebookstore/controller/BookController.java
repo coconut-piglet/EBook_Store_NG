df8865402e65359c3f0b0e3e21c6c415a91d753c
YUENIIMAA <yueniimaa@outlook.com>
2020-04-04 01:18:12 +0800

    add transaction management #2

--- src/main/java/se228/richard/ebookstore/controller/BookController.java
+++ src/main/java/se228/richard/ebookstore/controller/BookController.java
@@ -1,13 +1,17 @@
 package se228.richard.ebookstore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 import se228.richard.ebookstore.entity.Book;
 import se228.richard.ebookstore.entity.BookDetail;
 import se228.richard.ebookstore.entity.Message;
 import se228.richard.ebookstore.service.BookService;
+import se228.richard.ebookstore.service.WishListService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
@@ -24,8 +28,50 @@ public class BookController {
     //@Autowired
     //private BookService bookService;
 
+    private final static Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     WebApplicationContext applicationContext;
+
+    @RequestMapping(value = "/insertwishlist",method = RequestMethod.GET)
+    public Message insertWishList() throws RemoteException {
+        WishListService wishListService = applicationContext.getBean(WishListService.class);
+        wishListService.addBooksToWishList("BOOK_A", "BOOK_B", "BOOK_C");
+        Assert.isTrue(wishListService.getWishList().size() == 3, "First adding should work just fine");
+        logger.info("BOOK_A, BOOK_B, BOOK_C have been added to wishlist");
+        try {
+            wishListService.addBooksToWishList("BOOK_D", "BOOK_WITH_A_LONG_NAME");
+        } catch (RuntimeException e) {
+            logger.info("v--- The following exception is expect because 'BOOK_WITH_A_LONG_NAME' is too " +
+                    "big for the DB ---v");
+            logger.error(e.getMessage());
+        }
+
+        for (String book : wishListService.getWishList()) {
+            logger.info("So far, " + book + " have been added to wishlist");
+        }
+        logger.info("You shouldn't see BOOK_D or BOOK_WITH_A_LONG_NAME. BOOK_WITH_A_LONG_NAME violated DB constraints, " +
+                "and BOOK_D was rolled back in the same TX");
+        Assert.isTrue(wishListService.getWishList().size() == 3,
+                "'BOOK_WITH_A_LONG_NAMEl' should have triggered a rollback");
+
+        try {
+            wishListService.addBooksToWishList("BOOK_E", null);
+        } catch (RuntimeException e) {
+            logger.info("v--- The following exception is expect because null is not " +
+                    "valid for the DB ---v");
+            logger.error(e.getMessage());
+        }
+
+        for (String book : wishListService.getWishList()) {
+            logger.info("So far, " + book + " have been added to wishlist");
+        }
+        logger.info("You shouldn't see BOOK_E or null. null violated DB constraints, and " +
+                "BOOK_E was rolled back in the same TX");
+        Assert.isTrue(wishListService.getWishList().size() == 3,
+                "'null' should have triggered a rollback");
+        return new Message("SUCCESS", "Wishlist updated");
+    }
 
     @RequestMapping(value = "/fetchlibrary",method = RequestMethod.GET)
     public List<Book> fetchLibrary() throws RemoteException {
--- src/main/java/se228/richard/ebookstore/service/WishListService.java
+++ src/main/java/se228/richard/ebookstore/service/WishListService.java
@@ -1,4 +1,37 @@
 package se228.richard.ebookstore.service;
 
-public interface WishListService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Service
+public class WishListService {
+
+    private final static Logger logger = LoggerFactory.getLogger(WishListService.class);
+
+    public final JdbcTemplate jdbcTemplate;
+
+    public WishListService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    //@Transactional(propagation = Propagation.REQUIRES_NEW)
+    //@Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void addBooksToWishList(String... books) {
+        for (String book : books) {
+            logger.info("Add " + book + " to the wish list.");
+            jdbcTemplate.update("insert into wishlist(bookname) values (?)", book);
+        }
+    }
+
+    public List<String> getWishList() {
+        return jdbcTemplate.query("select bookname from wishlist", (rs, rowNum) -> rs.getString("bookname"));
+    }
 }
